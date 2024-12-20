97e4b954e4f484f7f355b7df87a3ba0783c4b9bd
Vinayak <nayakvinayak95@gmail.com>
2024-12-20 16:49:13 +0530

    Committing parlant sample tutorial project

---
+++ parlant-tutorial/basic-client/client.py
@@ -0,0 +1,35 @@
+from parlant.client import ParlantClient
+
+SERVER_ADDRESS="http://localhost:8800"
+client = ParlantClient(base_url=SERVER_ADDRESS)
+
+AGENT_ID = "ko8buqXQXE"
+agent = client.agents.retrieve(AGENT_ID)
+
+session = client.sessions.create(agent_id=AGENT_ID, allow_greeting=False)
+
+while True:
+     customer_message = input("Customer: ")
+
+     if customer_message in ("quit", "bye", "exit", "cya l8r m8"):
+         break
+
+     customer_event = client.sessions.create_event(
+         session_id=session.id,
+         source="customer",
+         kind="message",
+         message=customer_message,
+     )
+
+     agent_event, *_ = client.sessions.list_events(
+         session_id=session.id,
+         source="ai_agent",
+         kinds="message",
+         min_offset=customer_event.offset,
+     )
+
+     # Let type-checker know we do have data here
+     assert agent_event.data
+     agent_message = agent_event.data["message"]
+
+     print(f"Agent: {agent_message}")
---
+++ parlant-tutorial/tool-service/parlant_tool_service_starter/service.py
@@ -0,0 +1,62 @@
+import asyncio
+from enum import Enum
+import json
+
+from parlant.sdk import (
+    PluginServer,
+    ToolContext,
+    ToolResult,
+    tool,
+)
+
+with open("products.json", 'r') as file:
+    database = json.load(file)
+
+class ProductType(Enum):
+    STUDENT = "Student"
+    HNI = "HNI"
+    MASS = "Mass"
+
+@tool
+async def get_products_by_type(
+    context: ToolContext,
+    product_type: ProductType,
+) -> ToolResult:
+    """Get all products that match the specified product type"""
+    products = [item for item in database if item['type'] == product_type.value]
+    return ToolResult({"available_products": products})
+
+@tool
+async def inquire_eligibility(
+    context: ToolContext,
+    minimum_account_balance: float,
+) -> ToolResult:
+    """Get all products which the inquirer is eligible for subject to the minimum account balance condition"""
+    products = [item for item in database if item['minimum_account_balance'] >= minimum_account_balance]
+    return ToolResult({"eligible_for": products})
+
+@tool
+async def report_incident(
+    context: ToolContext,
+    incident_type: str,
+) -> ToolResult:
+    """Divert the incident to a human who specializes in handling grievances."""
+    if incident_type in ["fraud", "scam", "duped"]:
+        return ToolResult({"divert_to": "Fred"})
+    if incident_type in ["misplaced", "lost", "can't find"]:
+        return ToolResult({"divert_to": "Mitchell"})
+    if incident_type in ["fraud", "scam", "duped"]:
+        return ToolResult({"divert_to": "Sam"})
+
+TOOLS = [
+    get_products_by_type,
+    inquire_eligibility,
+    report_incident,
+]
+
+async def main() -> None:
+    async with PluginServer(tools=TOOLS, port=8089):
+        pass
+
+if __name__ == "__main__":
+    asyncio.run(main())
